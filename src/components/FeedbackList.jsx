131258bf6b2cb4f44ae285d4e8b84c268c7c1174
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2021-12-09 23:19:29 +0700

    Card Component & Conditional Styles

---
+++ src/components/FeedbackList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+import FeedbackItem from './FeedbackItem'
+
+function FeedbackList({ feedback }) {
+  if (!feedback || !feedback.length) {
+    return <p>No Feedback Yet</p>
+  }
+  return (
+    <div>
+      <div className="feedback-list">
+        {feedback.map((item) => (
+          <FeedbackItem key={item.id} item={item} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  )
+}
+
+export default FeedbackList
