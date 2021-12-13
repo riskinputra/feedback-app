5b7174929b51f26d9ad85fd9934d51f1b0ea51e6
Riski Nugroho Putra <31657885+riskinputra@users.noreply.github.com>
2021-12-13 23:48:55 +0700

    react router dom

--- src/App.js
+++ src/App.js
@@ -1,36 +1,49 @@
-import { v4 as uuidv4 } from 'uuid'
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackForm from './components/FeedbackForm'
-import FeedbackStats from './components/FeedbackStats'
+import { v4 as uuidv4 } from "uuid";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import FeedbackData from './data/FeedbackData'
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackForm from "./components/FeedbackForm";
+import FeedbackStats from "./components/FeedbackStats";
 
-import { useState } from 'react'
+import AboutPage from "./pages/AboutPage";
+
+import FeedbackData from "./data/FeedbackData";
+
+import { useState } from "react";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
   const deleteFeedback = (id) => {
-    if (window.confirm('Are you sure want to delete?')) {
-      setFeedback(feedback.filter((item) => item.id !== id))
+    if (window.confirm("Are you sure want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
     }
-  }
+  };
 
   const addFeedback = (newFeedback) => {
-    newFeedback.id = uuidv4()
-    setFeedback([newFeedback, ...feedback])
-  }
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route exact path="/" element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          }>
+          </Route>
+
+          <Route path="/about" element={<AboutPage/>} />
+        </Routes>
       </div>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
